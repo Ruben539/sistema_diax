7fbd69eda8331dc6b1f93d7063445e8052519f7d
Ruben Flores <floresruen380@gmail.com>
2023-01-03 14:42:30 -0300

    Cambio de modificacion.

--- Helpers/modificar_usuario.php
+++ Helpers/modificar_usuario.php
@@ -8,62 +8,54 @@ if (empty($_SESSION['active'])) {
 	}
 
 
-if (!empty($_POST)) {
-	$alert = '';
-
-	if (empty($_POST['nombre']) || empty($_POST['correo']) || empty($_POST['usuario']) ||empty($_POST['pass']) ||empty($_POST['rol'])) {
-
-		$alert = '<p class = "msg_error">Debe llenar Todos los Campos</p>';
-
-	}else{
-				
-		$iduser  = $_POST['id'];
-		$nombre  = $_POST['nombre'];
-		$correo  = $_POST['correo'];
-		$usuario = $_POST['usuario'];
-		$pass    = md5($_POST['pass']);
-		$rol     = $_POST['rol'];
-
-//echo "SELECT * FROM usuario 
-
-		//WHERE(usuario = '$user' AND idusuario != $iduser) or (correo = '$email' AND idusuario != $iduser";
-//exit; sirve para ejectuar la consulta en mysql
-
-		$query = mysqli_query($conection,"SELECT * FROM usuarios WHERE
-		 	(usuario = '$usuario' AND idusuario != $iduser) or (correo = '$correo' AND idusuario != $iduser) AND estatus = 1");
-
-		$resultado = mysqli_fetch_array($query);
-		if (is_countable($resultado)) {
-			# es mejor usar una condicional que verifique la variable.Usar is_iterable()para PHP> = 7.1 o is_countable()para> = 7.3 en este escenario. Fragmento de arriba $resultado
-		}
-
-		if ($resultado > 0) {
-			$alert = '<p class = "msg_error">El correo o Usuario ya existe</p>';
-
+	if (!empty($_POST)) {
+		$alert = '';
+	
+		if (empty( $_POST['nombre']) || empty($_POST['usuario']) || empty($_POST['correo']) ||empty($_POST['rol'])) {
+	
+			$alert = '<p class = "msg_error">Debe llenar Todos los Campos</p>';
+	
 		}else{
-
-			if ($_POST['pass']) {
-				
-				$sql_update = mysqli_query($conection,"UPDATE usuarios SET 
-					 nombre = '$nombre',correo = '$correo', usuario = '$usuario',rol = '$rol', pass = '$pass'
-					 WHERE idusuario = $iduser");
-			}else{
-
-				$sql_update = mysqli_query($conection,"UPDATE usuarios SET 
-					 nombre = '$nombre',correo = '$correo', usuario = '$usuario',pass = '$pass', rol = '$rol'
-					 WHERE idusuario = $iduser");
-			}
-
+	
+			$iduser              = $_POST['id'];
+			$nombre              = $_POST['nombre'];
+			$usuario             = $_POST['usuario'];
+			$pass                = md5($_POST['pass']);
+			$correo              = $_POST['correo'];
+			$rol                 = $_POST['rol'];
+			
+	
+	
+	//echo "SELECT * FROM usuario
+	
+			//WHERE(usuario = '$user' AND idusuario != $iduser) or (correo = '$email' AND idusuario != $iduser";
+	//exit; sirve para ejectuar la consulta en mysql
+			$query = mysqli_query($conection,"SELECT * FROM usuarios
+				WHERE  idusuario != idusuario"
+			);
+	
+			$resultado = mysqli_fetch_array($query);
+	
+	
+		}
+	
+		if ($resultado > 0) {
+			$alert = '<p class = "msg_error">El Registro ya existe,ingrese otro</p>';
+	
+		}else{
+	
+			$sql_update = mysqli_query($conection,"UPDATE usuarios SET nombre = '$nombre',usuario = '$usuario', pass = '$pass',correo = '$correo',rol = '$rol', estatus = 1
+				WHERE idusuario = $idusuario");
+	
 			if ($sql_update) {
-				$alert = '<p class = "alert alert-success">Usuario Actualizado</p>';
-				
+	
+				header('location: ../Plantillas/usuarios.php');
+	
 			}else{
-				$alert = '<p class = "msg_error">Error al Actualizar el Usuario</p>';
+				$alert = '<p class = "msg_error">Error al Actualizar el Registro</p>';
 			}
 		}
 	}
-
- }
 
 //Recuperacion de datos para mostrar al seleccionar Actualizar
 
@@ -96,15 +88,6 @@ if ($resultado == 0) {
 		$pass     =md5($data['pass']);
 		$rol      = $data['rol'];
 
-		if ($rol == 1) {
-			$option = '<option value="'.$rol.'" select>'.$rol.'</option>';
-		}else if ($rol == 2) {
-			$option = '<option value="'.$rol.'" select>'.$rol.'</option>';
-		}else if ($rol == 3) {
-			$option = '<option value="'.$rol.'" select>'.$rol.'</option>';
-		}else if ($rol == 4) {
-			$option = '<option value="'.$rol.'" select>'.$rol.'</option>';
-		}
 	}
 }
 
@@ -120,7 +103,7 @@ require_once("../body/header_admin.php");
               <form action="" method="POST">
                   <input type="hidden" name="id" id="id" value="<?php echo $iduser; ?>">
                 <div class="form-group">
-                  <input class="form-control" type="hidden" name="" id="" placeholder="Ingrese el nombre" 
+                  <input class="form-control" type="hidden" name="nombre" id="nombre" placeholder="Ingrese el nombre" 
                    value="<?php echo $nombre; ?>">
                 </div>
                 <div class="form-group">
@@ -130,7 +113,7 @@ require_once("../body/header_admin.php");
                 </div>
                 <div class="form-group">
                   <label class="control-label">Correo</label>
-                  <input class="form-control" type="email" name="correo" id="correo" placeholder="Ingrese el Nro de Vehiculo" required
+                  <input class="form-control" type="email" name="correo" id="correo" placeholder="Ingrese el coreo" required
                   value="<?php echo $correo; ?>">
                 </div>
                 <div class="form-group">
@@ -149,11 +132,10 @@ require_once("../body/header_admin.php");
                    <?php if ($_SESSION['rol'] == 1) {?>
                     <option value="1">Administrador</option>
                    <?php } ?>
-                   <option value="2">Administrador</option>
-                   <option value="9">Recepcionista</option>
+                   <option value="2">Recepcionista</option>
                    <option value="3">Doctor</option>
                    <option value="4">Paciente</option>
-                   <
+                   
                  </select>
                 </div>
                                   
