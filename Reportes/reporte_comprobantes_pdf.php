bb3c0c6f21ce295875c05f7c67a06835617a7be6
Ruben Flores <floresruen380@gmail.com>
2023-01-16 13:44:18 -0300

    Reporte de Rendicion del Comprobante.

--- Reportes/reporte_comprobantes_pdf.php
+++ Reportes/reporte_comprobantes_pdf.php
@@ -1,89 +1,110 @@
 <?php
 session_start();
-if ($_SESSION['rol'] == 1 || $_SESSION['rol'] == 2){
-    if (empty($_SESSION['active'])) {
+if ($_SESSION['rol'] == 1 || $_SESSION['rol'] == 2) {
+  if (empty($_SESSION['active'])) {
     header('location: salir.php');
-}
-
-}else{
-  header('location: salir.php');  
+  }
+} else {
+  header('location: salir.php');
 }
 require_once("../Modelos/conexion.php");
-  ob_start();
+ob_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="http://<?php echo $_SERVER['HTTP_HOST'];?>/sistema_diax/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/sistema_diax/bootstrap/dist/css/bootstrap.min.css">
   <title>Reporte de Comprobantes</title>
 </head>
+
 <body>
 
-<H2 class="text-center">Listado de Comprobantes del Dia</H2>
-<table  class="table table-striped table-bordered table-condensed" >
+  <H2 class="text-center">Listado de Comprobantes del Dia</H2>
+  <table class="table table-striped table-bordered table-condensed">
 
-<thead>
-                    <tr class="text-center">
-                            <th>ID</th>
-                            <th>Estudio</th>
-                            <th>Sin Seguro</th>
-                            <th>Semei</th>
-                            <th>Semei Preferencial</th>                                
-                            <th>Seguros</th>                                
-                            <th>Seguros Preferencial</th>                                
-                            <th>Hospitalario</th>                                
-                            <th>Fecha</th>                                
-                   
-                        </tr>
-                  </thead>
+    <thead>
+      <tr class="text-center">
+        <th>ID</th>
+        <th>Estudio</th>
+        <th>Sin Seguro</th>
+        <th>Semei</th>
+        <th>Semei Preferencial</th>
+        <th>Seguros</th>
+        <th>Seguros Preferencial</th>
+        <th>Hospitalario</th>
+        <th>Fecha</th>
 
-                  <tbody>
-                      <?php 
-                     $fecha =  date('d-m-Y');
-                    //  echo $fecha1." ".$fecha2;
-                    //  exit;
-                        $sql = mysqli_query($conection,"SELECT h.id,h.Estudio,h.Cedula,h.Atendedor,h.Fecha,h.Seguro,h.Monto,h.Comentario, h.fecha_2 FROM historial h 
+      </tr>
+    </thead>
+
+    <tbody>
+      <?php
+      $fecha =  date('d-m-Y');
+      //  echo $fecha1." ".$fecha2;
+      //  exit;
+      $sql = mysqli_query($conection, "SELECT h.id,h.Estudio,h.Cedula,h.Atendedor,h.Fecha,h.Seguro,h.Monto,h.Comentario, h.fecha_2 FROM historial h 
                         where  Fecha like '%$fecha%'   ORDER BY  h.id DESC");
 
-                         $resultado = mysqli_num_rows($sql);
+      $resultado = mysqli_num_rows($sql);
 
-                         if($resultado > 0){ 
-                            while ($data = mysqli_fetch_array($sql)){ 
-                      ?>
-                        <tr class="text-center">
-                             <td><?php echo $data['id'];?></td>
-                             <td><?php echo $data['Estudio'];?></td>
- 						                 <td><?php echo $data['Cedula'];?></td>
- 						                 <td><?php echo $data['Atendedor'];?></td>
- 						                 <td><?php echo $data['Fecha']; ?></td>
-                             <td><?php echo $data['Seguro']?></td>
-                             <td><?php echo $data['Monto']?></td>
-                             <td><?php echo $data['Comentario']?></td>
-                             <td><?php echo $data['fecha_2']?></td>
-                         
-                        </tr>
-                     
-                 
-                  <?php }
-                } ?>
-                 </tbody>
-                </table>
+      if ($resultado > 0) {
+        $monto = 0;
+        while ($data = mysqli_fetch_array($sql)) {
+          $monto += $data['Monto'];
+      ?>
+          <tr class="text-center">
+            <td><?php echo $data['id']; ?></td>
+            <td><?php echo $data['Estudio']; ?></td>
+            <td><?php echo $data['Cedula']; ?></td>
+            <td><?php echo $data['Atendedor']; ?></td>
+            <td><?php echo $data['Fecha']; ?></td>
+            <td><?php echo $data['Seguro'] ?></td>
+            <td><?php echo $data['Monto'] ?></td>
+            <td><?php echo $data['Comentario'] ?></td>
+            <td><?php echo $data['fecha_2'] ?></td>
+
+          </tr>
+
+
+
+    </tbody>
+    <tfoot>
+      <tr>
+        <td><b>Total A Rendir : </b></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td class="alert alert-success text-center"><?php echo $monto; ?>.000GS</td>
+        <td></td>
+        <td></td>
+        <td></td>
+
+      </tr>
+    </tfoot>
+<?php }
+      } ?>
+  </table>
 </body>
+
 </html>
 <?php
-  $html = ob_get_clean();
-  //echo $html;
+$html = ob_get_clean();
+//echo $html;
 
- require_once "../Library/dompdf/autoload.inc.php";
- use Dompdf\Dompdf;
- $dompdf = new Dompdf();
+require_once "../Library/dompdf/autoload.inc.php";
 
- $options = $dompdf->getOptions();
- $options->set(array('isRemoteEnabled' => true));
- $dompdf->setOptions($options);
+use Dompdf\Dompdf;
+
+$dompdf = new Dompdf();
+
+$options = $dompdf->getOptions();
+$options->set(array('isRemoteEnabled' => true));
+$dompdf->setOptions($options);
 
 $dompdf->loadHtml($html);
 
