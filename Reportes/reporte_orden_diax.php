48771f8db6299790d99be3906f98353d05efa922
Ruben Flores <floresruen380@gmail.com>
2023-01-19 11:40:59 -0300

    Reporte PDF de los gastos.

--- Reportes/reporte_orden_diax.php
+++ Reportes/reporte_orden_diax.php
@@ -57,11 +57,11 @@ ob_start();
                 FROM historial h inner join clientes c on c.cedula = h.cedula where  h.Fecha like '%$fecha%' AND h.Atendedor like '%DIAX%'  ORDER BY  h.id ASC");
 
                 $resultado = mysqli_num_rows($sql);
-                $monto = 0;
+                $diax = 0;
 
                 if ($resultado > 0) {
                   while ($data = mysqli_fetch_array($sql)) {
-                    $monto += (int)$data['Monto'];
+                    $diax += (int)$data['Monto'];
 
                 ?>
                     <tr class="text-center">
@@ -95,7 +95,7 @@ ob_start();
                 <td></td>
                 <td></td>
                 <td class="alert alert-success text-center">
-                <?php echo number_format($monto, 3, '.', '.'); ?>.<b>G</b>
+                <?php echo number_format($diax, 3, '.', '.'); ?>.<b>G</b>
                 </td>
 
 
@@ -105,7 +105,93 @@ ob_start();
         </div>
       </div>
     </div>
+    
+    <div class="row">
+    <div class="col-md-12">
+      <div class="tile">
+        <div class="table-responsive">
+          <table class="table table-striped table-bordered table-condensed" style="width:100%">
+            <thead>
+              <tr class="text-center">
+                <th></th>
+                <th></th>
+                <th>Fecha</th>
+                <th></th>
+                <th></th>
+                <th>Descripcion</th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th>Monto</th>
 
+              </tr>
+            </thead>
+
+            <tbody>
+              <?php
+              // $fecha1 = "05-01-2023";
+              $fecha =  date('Y-m-d');
+              //  echo $fecha1." ".$fecha2;
+              //  exit;
+              $sql = mysqli_query($conection, "SELECT g.id,g.descripcion,g.monto,g.created_at  FROM gastos g 
+               where  g.created_at like '%$fecha%'   ORDER BY  g.id ASC");
+
+              $resultado = mysqli_num_rows($sql);
+              $gasto = 0;
+
+              if ($resultado > 0) {
+                while ($data = mysqli_fetch_array($sql)) {
+                  $gasto += (int)$data['monto'];
+
+              ?>
+                  <tr class="text-center">
+                   <td></td>
+                    <td></td>
+                    <td><?php echo $data['created_at'] ?></td>
+                    <td></td>
+                    <td></td>
+                    <td><?php echo $data['descripcion']; ?></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><?php echo $data['monto']; ?></td>
+
+                  </tr>
+
+
+              <?php }
+              } ?>
+            </tbody>
+            <tr>
+              <td><b>Total A Gastos : </b></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td class="alert alert-success text-center">
+                <?php echo number_format($gasto, 3, '.', '.'); ?>.<b>GS</b>
+              </td>
+
+
+            </tr>
+          </table>
+        <section>
+          <?php
+            $rendicion = $diax - $gasto;
+          ?>
+          <p>Rencion Final</p>
+          <p style="text-align: right;" class="alert alert-danger"> <?php echo number_format($rendicion, 3, '.', '.'); ?>.<b>GS</b></p>
+        </section>
+             
+
+        </div>
+      </div>
+    </div>
+  </div
 
   </main>
 </body>
