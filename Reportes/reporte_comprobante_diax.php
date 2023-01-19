1db9a369d5bc337b445d55533d9bdac438daf68d
Ruben Flores <floresruen380@gmail.com>
2023-01-19 11:45:19 -0300

    Generacion de Reportes. Para el Control diario en PDF Y EXCEL.

--- Reportes/reporte_comprobante_diax.php
+++ Reportes/reporte_comprobante_diax.php
@@ -29,16 +29,16 @@ header("Expires: 0");
               <thead>
                 <tr class="text-center">
 
-                <th>Fecha</th>
-                <th>Nombre</th>
-                <th>Cedula</th>
-                <th>Estudio</th>
-                <th>Doctor/a</th>
-                <th>Seguro</th>
-                <th>Monto</th>
-                <th>Monto Seguro</th>
-                <th>Descuento</th>
-                <th>Comentario</th>
+                  <th>Fecha</th>
+                  <th>Nombre</th>
+                  <th>Cedula</th>
+                  <th>Estudio</th>
+                  <th>Doctor/a</th>
+                  <th>Seguro</th>
+                  <th>Monto</th>
+                  <th>Monto Seguro</th>
+                  <th>Descuento</th>
+                  <th>Comentario</th>
 
                 </tr>
               </thead>
@@ -63,16 +63,16 @@ header("Expires: 0");
                 ?>
                     <tr class="text-center">
 
-                    <td><?php echo $data['Fecha'] ?></td>
-                    <td><?php echo $data['nombre']; ?></td>
-                    <td><?php echo $data['Cedula']; ?></td>
-                    <td><?php echo $data['Estudio']; ?></td>
-                    <td><?php echo $data['Atendedor']; ?></td>
-                    <td><?php echo $data['Seguro']; ?></td>
-                    <td><?php echo $data['Monto'] ?></td>
-                    <td><?php echo $data['MontoS'] ?></td>
-                    <td><?php echo $data['Descuento'] ?></td>
-                    <td><?php echo $data['Comentario'] ?></td>
+                      <td><?php echo $data['Fecha'] ?></td>
+                      <td><?php echo $data['nombre']; ?></td>
+                      <td><?php echo $data['Cedula']; ?></td>
+                      <td><?php echo $data['Estudio']; ?></td>
+                      <td><?php echo $data['Atendedor']; ?></td>
+                      <td><?php echo $data['Seguro']; ?></td>
+                      <td><?php echo $data['Monto'] ?></td>
+                      <td><?php echo $data['MontoS'] ?></td>
+                      <td><?php echo $data['Descuento'] ?></td>
+                      <td><?php echo $data['Comentario'] ?></td>
 
 
                     </tr>
@@ -92,12 +92,99 @@ header("Expires: 0");
                 <td></td>
                 <td></td>
                 <td class="alert alert-success text-center">
-                <?php echo number_format($monto, 3, '.', '.'); ?>.<b>G</b>
+                  <?php echo number_format($monto, 3, '.', '.'); ?>.<b>G</b>
                 </td>
 
 
               </tr>
             </table>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-12">
+        <div class="tile">
+          <div class="table-responsive">
+            <table class="table table-striped table-bordered table-condensed" style="width:100%">
+              <thead>
+                <tr class="text-center">
+                  <th></th>
+                  <th></th>
+                  <th>Fecha</th>
+                  <th></th>
+                  <th></th>
+                  <th>Descripcion</th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th>Monto</th>
+
+                </tr>
+              </thead>
+
+              <tbody>
+                <?php
+                // $fecha1 = "05-01-2023";
+                $fecha =  date('Y-m-d');
+                //  echo $fecha1." ".$fecha2;
+                //  exit;
+                $sql = mysqli_query($conection, "SELECT g.id,g.descripcion,g.monto,g.created_at  FROM gastos g 
+                where  g.created_at like '%$fecha%'   ORDER BY  g.id ASC");
+
+                $resultado = mysqli_num_rows($sql);
+                $gasto = 0;
+
+                if ($resultado > 0) {
+                  while ($data = mysqli_fetch_array($sql)) {
+                    $gasto += (int)$data['monto'];
+
+                ?>
+                    <tr class="text-center">
+                      <td></td>
+                      <td></td>
+                      <td><?php echo $data['created_at'] ?></td>
+                      <td></td>
+                      <td></td>
+                      <td><?php echo $data['descripcion']; ?></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td><?php echo $data['monto']; ?></td>
+
+                    </tr>
+
+
+                <?php }
+                } ?>
+              </tbody>
+              <tr>
+                <td><b>Total A Gastos : </b></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td class="alert alert-success text-center">
+                  <?php echo number_format($gasto, 3, '.', '.'); ?>.<b>GS</b>
+                </td>
+
+
+              </tr>
+            </table>
+            <section>
+              <?php
+              $rendicion = $diax - $gasto;
+              ?>
+              <p><b>Rendicion Final</b></p>
+              <p style="text-align: right;" class="alert alert-danger"> <?php echo number_format($rendicion, 3, '.', '.'); ?>.<b>GS</b></p>
+            </section>
+
+
           </div>
         </div>
       </div>
