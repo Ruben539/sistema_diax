0925c85d12ddce0837d2bef2a761737754b3c330
Ruben Flores <floresruen380@gmail.com>
2023-01-30 11:04:53 -0300

    Monto de Total de ingreso

--- Reportes/reporte_comprobantes_pdf.php
+++ Reportes/reporte_comprobantes_pdf.php
@@ -130,7 +130,6 @@ ob_start();
                 <td></td>
                 <td></td>
                 <td></td>
-                <td></td>
                 <td class="alert alert-success text-center">
                 <?php echo number_format($monto, 3, '.', '.'); ?>.<b>G</b>
                 </td>
