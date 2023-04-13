cab8e7c3a8eeeb1e0ff7ed165b84f328fe0a1139
Ruben Flores <78937675+Ruben539@users.noreply.github.com>
2023-04-13 15:09:12 -0400

    Cambio en la consulta. Para la asignacion de Informantes.

--- Historial/PendientesAsignacionFabiola.php
+++ Historial/PendientesAsignacionFabiola.php
@@ -50,7 +50,7 @@ require_once("../body/header_admin.php");
 
                   <tbody>
                       <?php 
-                        $hoy = date('m-Y');
+                        $hoy = date('Y');
                         
                           $sql = mysqli_query($conection, "SELECT h.id,h.Cedula,c.nombre,c.apellido,h.estudio,h.atendedor,h.placas,h.informa,h.fecha FROM historial h INNER JOIN clientes c on c.cedula = h.cedula
                          WHERE atendedor like '%Fabiola%'  AND  fecha like '%".$hoy."%' ORDER BY ID ASC ");
