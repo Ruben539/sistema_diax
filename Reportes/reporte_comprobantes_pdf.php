6d89e8be8c5b73a64bb6144feb50d44156cb3fbc
Ruben Flores <78937675+Ruben539@users.noreply.github.com>
2023-04-13 14:43:37 -0400

    Reporte de Comprobantes. Agregado de lod gastos en el reporte.

--- Reportes/reporte_comprobantes_pdf.php
+++ Reportes/reporte_comprobantes_pdf.php
@@ -142,6 +142,109 @@ ob_start();
       </div>
     </div>
 
+    <div class="row">
+    <div class="col-md-12">
+      <div class="tile">
+        <div class="table-responsive">
+          <table class="table table-striped table-bordered table-condensed" style="width:100%">
+            <thead>
+              <tr class="text-center">
+                <th></th>
+                <th></th>
+                <th>Fecha</th>
+                <th></th>
+                <th></th>
+                <th>Descripcion</th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th>Monto</th>
+
+              </tr>
+            </thead>
+
+            <tbody>
+              <?php
+              
+              $fecha_desde =  date_create($_REQUEST['fecha_desde']);;
+              $desde = date_format($anio, 'm-Y');
+              $hoy = date("Y-m-d");
+              
+              $anio = date_create($_REQUEST['fecha_desde']);
+              $hoy = date_format($anio, 'm-Y');
+              
+              if (empty($_POST['fecha_desde']) && empty($_POST['fecha_hasta']) ) {
+              
+               
+                  $sql = mysqli_query($conection, "SELECT g.id,g.descripcion,g.monto,g.created_at
+                  FROM gastos g  where created_at like '%".$hoy."%'  AND g.estatus = 1 ");
+              
+              }else{ 
+              
+                  $fecha_desde = $_POST['fecha_desde'];
+                  $fecha_hasta  = $_POST['fecha_hasta'];
+                 // exit();
+              
+                  $sql = mysqli_query($conection, "SELECT g.id,g.descripcion,g.monto,g.created_at
+                FROM gastos g  where created_at BETWEEN '$fecha_desde' AND '$fecha_hasta' AND g.estatus = 1");
+              }
+
+              $resultado = mysqli_num_rows($sql);
+              $gasto = 0;
+
+              if ($resultado > 0) {
+                while ($data = mysqli_fetch_array($sql)) {
+                  $gasto += number_format($data['monto'], 3,'.', '.');
+
+              ?>
+                  <tr class="text-center">
+                   <td></td>
+                    <td></td>
+                    <td><?php echo $data['created_at'] ?></td>
+                    <td></td>
+                    <td></td>
+                    <td><?php echo $data['descripcion']; ?></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><?php echo $data['monto']; ?></td>
+
+                  </tr>
+
+
+              <?php }
+              } ?>
+            </tbody>
+            <tr>
+              <td><b>Total A Gastos : </b></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td class="alert alert-success text-center">
+                <?php echo number_format($gasto, 3, '.', '.'); ?>.<b>GS</b>
+              </td>
+
+
+            </tr>
+          </table>
+        <section>
+          <?php
+            $rendicion = $monto - $gasto;
+          ?>
+          <p>Rencion Final</p>
+          <p style="text-align: right;" class="alert alert-danger"> <?php echo number_format($rendicion, 3, '.', '.'); ?>.<b>GS</b></p>
+        </section>
+             
+
+        </div>
+      </div>
+    </div>
+  </div>
 
   </main>
 </body>
